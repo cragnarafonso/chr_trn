cb429c246a230af3e7c8bebe0279a7a0116c7084
cragnarafonso <christopheacga@gmail.com>
2024-05-07 16:02:36 +0100

    First Commit

---
+++ trn_records/src/main/java/cragnarafonso/pt/trn_records/App.java
@@ -0,0 +1,14 @@
+package cragnarafonso.pt.trn_records;
+
+/**
+ * Hello world!
+ *
+ */
+@SuppressWarnings("javadoc")
+public class App 
+{
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" ); //$NON-NLS-1$
+    }
+}
---
+++ trn_records/src/test/java/cragnarafonso/pt/trn_records/AppTest.java
@@ -0,0 +1,16 @@
+package cragnarafonso.pt.trn_records;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+@SuppressWarnings({"javadoc", "static-method"})
+public class AppTest 
+{
+	@Test
+    public void test()
+    {
+        System.out.println("Hello World"); //$NON-NLS-1$
+    }
+}
